9834243dea8940a99b8ccde0f8349eb4d982c62d
manmohan chauhan <manmohanchauhan777@gmail.com>
2020-10-19 12:31:52 +0530

    updated README.md

--- app/src/main/java/com/manmohan/rgbColorPalette/PaletteView.java
+++ app/src/main/java/com/manmohan/rgbColorPalette/PaletteView.java
@@ -1,6 +1,5 @@
 package com.manmohan.rgbColorPalette;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -9,7 +8,6 @@ import android.graphics.RectF;
 import android.graphics.Shader;
 import android.graphics.SweepGradient;
 import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -77,8 +75,9 @@ class PaletteView extends View {
         cornerCirclePaint.setStrokeWidth(STROKE_WIDTH / 8f);
         cornerCirclePaint.setStyle(Paint.Style.STROKE);
         cornerCirclePaint.setAntiAlias(true);
-        if (STROKE_WIDTH >= 80)
+        if (STROKE_WIDTH >= 80) {
             cornerCirclePaint.setShadowLayer(10f, 2.0f, 2.0f, 0x80000000);
+        }
 
 
         colorPalettePaint = new Paint();
@@ -94,11 +93,10 @@ class PaletteView extends View {
         Log.e(TAG, "onSizeChanged: called");
         if (w < h) {
             cornerCircleX = w - VIEW_MARGIN;
-            cornerCircleY = h / 2f;
         } else {
             cornerCircleX = (((w - h) / 2f) + h) - VIEW_MARGIN;
-            cornerCircleY = h / 2f;
         }
+        cornerCircleY = h / 2f;
         oval = getRectangle(VIEW_MARGIN);
 
 
@@ -112,50 +110,57 @@ class PaletteView extends View {
         drawBulbImage(canvas);
     }
 
-    @SuppressLint("ClickableViewAccessibility")
-    void drawColorPallet(Canvas canvas) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Shader gradient = new SweepGradient(this.getWidth() / 2f, this.getHeight() / 2f, colors, positions);
-            colorPalettePaint.setShader(gradient);
-            colorPalettePaint.setStyle(Paint.Style.STROKE);
-            colorPalettePaint.setStrokeWidth(STROKE_WIDTH);
-
-
-            canvas.drawArc(oval, 0f, 360f, true, colorPalettePaint);
-
-            this.setOnTouchListener(new OnTouchListener() {
-                @Override
-                public boolean onTouch(View v, MotionEvent event) {
-                    final float evX = event.getX();
-                    final float evY = event.getY();
-                    double clickDistance = Math.sqrt(Math.pow(evX - getPaletteCenterX(), 2) + Math.pow(evY - getPaletteCenterY(), 2));
-                    float innerRadius = (mPaletteDiameter / 2f) - (STROKE_WIDTH / 2f);
-                    float outerRadius = (mPaletteDiameter / 2f) + (STROKE_WIDTH / 2f);
-                    switch (event.getAction()) {
-                        case MotionEvent.ACTION_DOWN:
-                            if ((clickDistance > innerRadius) && (clickDistance < outerRadius)) {
-                                pointerMoving = true;
-                                updateCornerCircleLocation(evX, evY);
-
-                            }
-                            Log.e(TAG, "onTouch: " + MotionEvent.ACTION_DOWN);
-                            break;
-                        case MotionEvent.ACTION_MOVE:
-                            if(pointerMoving){
-                                updateCornerCircleLocation(evX, evY);
-                            }
-                            break;
-                        case MotionEvent.ACTION_UP:
-                            pointerMoving = false;
-                            break;
-                    }
-                    return true;
-                }
-
-
-            });
-        }
+    @Override
+    public boolean performClick() {
+        return super.performClick();
     }
+
+    void drawColorPallet(Canvas canvas) {
+        Shader gradient = new SweepGradient(this.getWidth() / 2f, this.getHeight() / 2f, colors, positions);
+        colorPalettePaint.setShader(gradient);
+        colorPalettePaint.setStyle(Paint.Style.STROKE);
+        colorPalettePaint.setStrokeWidth(STROKE_WIDTH);
+
+
+        canvas.drawArc(oval, 0f, 360f, true, colorPalettePaint);
+
+        this.setOnTouchListener(paletteTouchListener);
+    }
+
+
+
+
+
+
+    OnTouchListener paletteTouchListener= new OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            final float evX = event.getX();
+            final float evY = event.getY();
+            double clickDistance = Math.sqrt(Math.pow(evX - getPaletteCenterX(), 2) + Math.pow(evY - getPaletteCenterY(), 2));
+            float innerRadius = (mPaletteDiameter / 2f) - (STROKE_WIDTH / 2f);
+            float outerRadius = (mPaletteDiameter / 2f) + (STROKE_WIDTH / 2f);
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    if ((clickDistance > innerRadius) && (clickDistance < outerRadius)) {
+                        pointerMoving = true;
+                        updateCornerCircleLocation(evX, evY);
+
+                    }
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    if (pointerMoving) {
+                        updateCornerCircleLocation(evX, evY);
+                    }
+                    break;
+                case MotionEvent.ACTION_UP:
+                    pointerMoving = false;
+                    performClick();
+                    break;
+            }
+            return true;
+        }
+    };
 
     private void drawColorCircle(Canvas canvas) {
 
@@ -189,78 +194,17 @@ class PaletteView extends View {
         invalidate();
     }
 
-
-    public void setStrokeWidth(int w) {
-        STROKE_WIDTH = w;
-        invalidate();
-    }
-
-    public void setCENTER_IMAGE_DRAWABLE(Drawable drawable) {
-        CENTER_IMAGE_DRAWABLE = drawable;
-        invalidate();
-    }
-
     private void updateCornerCircleLocation(double x, double y) {
         findCircleCornerPointAndUpdateListener(getPaletteCenterX(), getPaletteCenterY(), x, y, (mPaletteDiameter / 2f));
     }
 
-    public void findCircleCornerPointAndUpdateListener(double x1, double y1, double x2, double y2, double diatanceOfThirdPoint) {
-
-//    calculate distance between the two points
-        double diatanceOfTwoPoints = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-        double x;
-        double y;
-        double T = diatanceOfThirdPoint / diatanceOfTwoPoints;
-        // Math.toRadians(30);
-        // finding point C coordinate
-        x = (1 - T) * x1 + T * x2;
-        y = (1 - T) * y1 + T * y2;
-//        for getting color of given x and y
-        try {
-            float touchAngle = (float) Math.atan2(y - y1, x - x1);
-
-            int mCurrentColor = calculateColor(touchAngle);
-            if (mCurrentColor != previousColor) {
-                // changeCircleColor(mCurrentColor);
-                mListener.onSelectXY(x, y);
-                if (mCurrentColor != Color.WHITE && (Color.red(mCurrentColor) == 255
-                        || Color.green(mCurrentColor) == 255
-                        || Color.blue(mCurrentColor) == 255)) {
-                    changeCircleColorWithXY(mCurrentColor, (float) x, (float) y);
-                    mListener.onColorSelected(mCurrentColor);
-                    previousColor = mCurrentColor;
-                } else {
-                    changeCircleXY((float) x, (float) y);
-                }
-            }
-        } catch (Exception avoid) {
-            Log.e(TAG, "findCircleCornerPointAndUpdateListener: " + avoid);
-        }
-    }
-
-
-    public void setListener(PaletteListener listener) {
-        mListener = listener;
-
-        // We'll start listening for touches now that the implementer cares about them
-        if (listener == null) {
-            setOnTouchListener(null);
-        }
-    }
-
-
-    public float getPaletteRadius() {
-        return mPaletteDiameter / 2f;
-    }
-
-    public float getPaletteCenterX() {
+    private float getPaletteCenterX() {
         return mView.getWidth() / 2f;
     }
 
-    public float getPaletteCenterY() {
+    private float getPaletteCenterY() {
         return mView.getHeight() / 2f;
     }
-
 
     private RectF getRectangle(int margin2) {
         int right, left, top, bottom, margin;
@@ -312,6 +256,60 @@ class PaletteView extends View {
     private int ave(int s, int d, float p) {
         return s + Math.round(p * (d - s));
     }
+    private void findCircleCornerPointAndUpdateListener(double x1, double y1, double x2, double y2, double diatanceOfThirdPoint) {
+
+//    calculate distance between the two points
+        double diatanceOfTwoPoints = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        double x;
+        double y;
+        double T = diatanceOfThirdPoint / diatanceOfTwoPoints;
+        // Math.toRadians(30);
+        // finding point C coordinate
+        x = (1 - T) * x1 + T * x2;
+        y = (1 - T) * y1 + T * y2;
+//        for getting color of given x and y
+        try {
+            float touchAngle = (float) Math.atan2(y - y1, x - x1);
+
+            int mCurrentColor = calculateColor(touchAngle);
+            if (mCurrentColor != previousColor) {
+                // changeCircleColor(mCurrentColor);
+                mListener.onSelectXY(x, y);
+                if (mCurrentColor != Color.WHITE && (Color.red(mCurrentColor) == 255
+                        || Color.green(mCurrentColor) == 255
+                        || Color.blue(mCurrentColor) == 255)) {
+                    changeCircleColorWithXY(mCurrentColor, (float) x, (float) y);
+                    mListener.onColorSelected(mCurrentColor);
+                    previousColor = mCurrentColor;
+                } else {
+                    changeCircleXY((float) x, (float) y);
+                }
+            }
+        } catch (Exception avoid) {
+            Log.e(TAG, "findCircleCornerPointAndUpdateListener: " + avoid);
+        }
+    }
+
+
+    public void setStrokeWidth(int w) {
+        STROKE_WIDTH = w;
+        invalidate();
+    }
+
+    public void setCENTER_IMAGE_DRAWABLE(Drawable drawable) {
+        CENTER_IMAGE_DRAWABLE = drawable;
+        invalidate();
+    }
+
+    public void setListener(PaletteListener listener) {
+        mListener = listener;
+
+        // We'll start listening for touches now that the implementer cares about them
+        if (listener == null) {
+            setOnTouchListener(null);
+        }
+    }
+
 
 
     public interface PaletteListener {
